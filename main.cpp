d04d3ffd4d130001aae70a936d8daff4a5e405ca
Jan Czajka <jan.czajka@web.de>
2018-02-25 09:57:19 +0100

    changed opengl version to 4.0

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(void) {
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
